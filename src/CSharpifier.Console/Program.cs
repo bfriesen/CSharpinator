a10ecb17f3a03fdce64fc9615fc42628447b7911
Brian Friesen <brianfriesen@quickenloans.com>
2013-09-11 13:20:23 -0400

    Clean up Program.cs

--- src/CSharpifier.Console/Program.cs
+++ src/CSharpifier.Console/Program.cs
@@ -10,43 +10,35 @@ namespace CSharpifier
         {
             XDocument xDocument;
 
-            if (args == null)
+            if (args.Length == 0)
             {
-                xDocument = XDocument.Parse(@"
-<foo>
-  <bars>
-    <bar id=""1"">abc</bar>
-    <bar id=""1"">xyz</bar>
-    <baz>true</baz>
-  </bars>
-</foo>");
-            }
-            else
-            {
-                if (args.Length == 0)
-                {
-                    Console.WriteLine("Must include path to document as first argument.");
-                    return;
-                }
-
-                if (!File.Exists(args[0]))
-                {
-                    Console.WriteLine("No file exists at: " + args[0]);
-                    return;
-                }
-
-                try
-                {
-                    xDocument = XDocument.Load(args[0]);
-                }
-                catch
-                {
-                    Console.WriteLine("Error reading into XDocument for file: " + args[0]);
-                    return;
-                }
+                Console.WriteLine("Must include path to document as first argument.");
+                return;
             }
 
-            //xDocument.Root.ToString().Dump(); "".Dump();
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("No file exists at: " + args[0]);
+                return;
+            }
+
+            try
+            {
+                xDocument = XDocument.Load(args[0]);
+            }
+            catch
+            {
+                Console.WriteLine("Error reading into XDocument for file: " + args[0]);
+                return;
+            }
+
+//            xDocument = XDocument.Parse(@"
+//            <foo>
+//              <bars>
+//                <bar>123</bar>
+//                <bar>2147483648</bar>
+//              </bars>
+//            </foo>");
 
             var domElement = new XmlDomElement(xDocument.Root);
 
@@ -54,15 +46,6 @@ namespace CSharpifier
 
             var domVisitor = new DomVisitor(classRepository);
             domVisitor.Visit(domElement);
-
-            //    var classes = classRepository.GetAll();
-            //    classes.Dump();
-            //
-            //    var classDefinitions = ClassDefinitions.FromClasses(classes);
-            //    classDefinitions.Dump();
-            //
-            //    var loadedClasses = classDefinitions.ToClasses(classRepository);
-            //    loadedClasses.Dump();
 
             var classGenerator = new ClassGenerator(classRepository);
             classGenerator.Write(
