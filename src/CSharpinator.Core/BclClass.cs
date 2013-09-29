678ea5cd20b3dfa6fccbfa9e33c2fb7876ae0dfc
Brian Friesen <brian.friesen@gmail.com>
2013-09-29 15:20:29 -0400

    Better detection of integer types.

--- src/CSharpinator.Core/BclClass.cs
+++ src/CSharpinator.Core/BclClass.cs
@@ -107,7 +107,7 @@ namespace CSharpinator
                         typeName,
                         "int",
                         value => { int temp; return int.TryParse(value, out temp); },
-                        value => value is int || value is uint || value is short || value is ushort || value is byte || value is sbyte));
+                        value => IsIntegerType(value) && IsLegalInt32Value(value)));
             }
         }
 
@@ -121,7 +121,7 @@ namespace CSharpinator
                         typeName,
                         "int?",
                         value => { int temp; return string.IsNullOrEmpty(value) || int.TryParse(value, out temp); },
-                        value => value == null || value is int || value is uint || value is short || value is ushort || value is byte || value is sbyte,
+                        value => value == null || (IsIntegerType(value) && IsLegalInt32Value(value)),
                         ".ToString()",
                         "int.Parse(value)"));
             }
@@ -136,7 +136,7 @@ namespace CSharpinator
                     typeName => new BclClass(
                         typeName,
                         "long", value => { long temp; return long.TryParse(value, out temp); },
-                        value => value is long || value is ulong || value is int || value is uint || value is short || value is ushort || value is byte || value is sbyte));
+                        value => IsIntegerType(value) && IsLegalInt64Value(value)));
             }
         }
 
@@ -150,7 +150,7 @@ namespace CSharpinator
                         typeName,
                         "long?",
                         value => { long temp; return string.IsNullOrEmpty(value) || long.TryParse(value, out temp); },
-                        value => value == null || value is long || value is ulong || value is int || value is uint || value is short || value is ushort || value is byte || value is sbyte,
+                        value => value == null || (IsIntegerType(value) && IsLegalInt64Value(value)),
                         ".ToString()",
                         "long.Parse(value)"));
             }
@@ -214,6 +214,51 @@ namespace CSharpinator
                         ".ToString()",
                         "Guid.Parse(value)"));
             }
+        }
+
+        private static bool IsIntegerType(object value)
+        {
+            return value is long || value is ulong || value is int || value is uint || value is short || value is ushort || value is byte || value is sbyte;
+        }
+
+        public static bool IsLegalInt32Value(object value)
+        {
+            return IsLegalInt32ValueImpl(value as dynamic);
+        }
+
+        private static bool IsLegalInt32ValueImpl(object value)
+        {
+            return false;
+        }
+
+        private static bool IsLegalInt32ValueImpl(long value)
+        {
+            return value >= int.MinValue && value <= int.MaxValue;
+        }
+
+        private static bool IsLegalInt32ValueImpl(ulong value)
+        {
+            return value <= int.MaxValue;
+        }
+
+        public static bool IsLegalInt64Value(object value)
+        {
+            return IsLegalInt64ValueImpl(value as dynamic);
+        }
+
+        private static bool IsLegalInt64ValueImpl(object value)
+        {
+            return false;
+        }
+
+        private static bool IsLegalInt64ValueImpl(long value)
+        {
+            return true;
+        }
+
+        private static bool IsLegalInt64ValueImpl(ulong value)
+        {
+            return value <= long.MaxValue;
         }
         #endregion
     }
