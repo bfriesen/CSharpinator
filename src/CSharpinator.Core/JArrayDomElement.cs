71919ff5ec9cb00fd4487b84b5f5cada4c03a400
Brian Friesen <brianfriesen@quickenloans.com>
2013-10-03 18:07:02 -0400

    JArrayDomElement can figure out the "best" JTokenType to use.

--- src/CSharpinator.Core/JArrayDomElement.cs
+++ src/CSharpinator.Core/JArrayDomElement.cs
@@ -20,25 +20,81 @@ namespace CSharpinator
 
         public bool HasElements
         {
-            get { return true; }
+            get { return false; }
         }
 
         public IEnumerable<IDomElement> Elements
         {
             get
             {
-                return _jArray.Select(item => _factory.CreateJsonDomElement(item, _name)); // TODO: singularize _name?
+                yield break;
             }
         }
 
         public Property CreateProperty(IClassRepository classRepository)
         {
+            var bestFitType = GetBestFitType();
+
+            // If JTokenType.None, then there weren't any elements.
+            // If JTokenType.Null, then no JTokenType is able to contain the values of all the other elements.
+            // Else, we have a JTokenType that can contain all the values of the other elements.
+
             throw new NotImplementedException();
         }
 
         public DomPath GetDomPath(IFactory factory)
         {
             return _jArray.GetDomPath(factory);
+        }
+
+        private JTokenType GetBestFitType()
+        {
+            if (_jArray.Count == 0)
+            {
+                return JTokenType.None;
+            }
+
+            var bestFitType =
+                _jArray
+                    .Select(candidate => new[] { candidate }.Concat(_jArray.Where(test => candidate != test)).ToList()).ToList()
+                    .Where(list => list.All(test => CanContain(list.First().Type, test.Type)))
+                    .SelectMany(x => x)
+                    .Select(x => (JTokenType?)x.Type)
+                    .FirstOrDefault();
+
+            return bestFitType.HasValue ? bestFitType.Value : JTokenType.Null;
+        }
+
+        private static bool CanContain(JTokenType candidate, JTokenType test)
+        {
+            switch (candidate)
+            {
+                case JTokenType.None:
+                case JTokenType.Constructor:
+                case JTokenType.Property:
+                case JTokenType.Comment:
+                case JTokenType.Undefined:
+                case JTokenType.Null:
+                case JTokenType.Bytes:
+                case JTokenType.Raw:
+                    return false;
+                case JTokenType.Object:
+                case JTokenType.Array:
+                case JTokenType.Integer:
+                case JTokenType.Boolean:
+                case JTokenType.Guid:
+                case JTokenType.Uri:
+                case JTokenType.TimeSpan:
+                case JTokenType.Date:
+                    return test == JTokenType.Null || test == candidate;
+                case JTokenType.Float:
+                    return test == JTokenType.Null || test == JTokenType.Float || test == JTokenType.Integer;
+                case JTokenType.String:
+                    // TODO: Fix this - it is probably slightly wrong.
+                    return test == JTokenType.Null || test == JTokenType.String || test == JTokenType.Date || test == JTokenType.Guid || test == JTokenType.TimeSpan;
+                default:
+                    throw new ArgumentOutOfRangeException("candidate");
+            }
         }
     }
 }
